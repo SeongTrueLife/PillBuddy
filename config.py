d09a5e70c40cb6aa38d87915d3d675785f07172d
Hello_New_World! <sch629@naver.com>
2025-11-16 13:00:59 +0900

    Refactor config to use Streamlit secrets

--- config.py
+++ config.py
@@ -1,11 +1,6 @@
-# config.py
+import streamlit as st
 
-# --- 부품 공장 3호 (성우) 열쇠 ---
-AZURE_SPEECH_KEY = "AqkebeXFVtC2NwCZn7itEPKi2TzV59y7UGmbk9Ew2gxUOYE4kFIDJQQJ99BKACNns7RXJ3w3AAAYACOGMTGg"
-AZURE_SPEECH_REGION = "koreacentral"
-
-# --- 부품 공장 1호 (e약은요) 열쇠 ---
-E_YAK_API_KEY = "461624567ca0d9b03d154b1807c58668d1a6c994fe79e380de7649f94240f536"
-
-# --- 부품 공장 2호 (작가) 열쇠 ---
-GEMINI_API_KEY = "AIzaSyCpWwYRMG_3QAoAr3IAuqAZlQLH-aIvDA4"
+AZURE_SPEECH_KEY = st.secrets["AZURE_SPEECH_KEY"]
+AZURE_SPEECH_REGION = st.secrets["AZURE_SPEECH_REGION"]
+E_YAK_API_KEY = st.secrets["E_YAK_API_KEY"]
+GEMINI_API_KEY = st.secrets["GEMINI_API_KEY"]
